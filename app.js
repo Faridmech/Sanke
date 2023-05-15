8fc8cd36847d995309be37ac1ee3d6b4d1b474b3
Faridmech <feridh189@gmail.com>
2023-05-15 16:56:05 +0400

    Finish

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersect } from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js"
+import { outsideGrid } from "./grid.js";
 
+
+const gameBoard = document.getElementById("game-board")
+
+let lastRenderTime = 0;
+let gameOver = false
+
+function main(currentTime){
+    if(gameOver){
+        if(confirm("You lost Game Press OK to restart")){
+            window.location = "/"
+        }
+        return
+    }
+
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRenderTime = (currentTime - lastRenderTime) / 1000;
+    if(secondsSinceLastRenderTime < 1 / SNAKE_SPEED) return
+    
+    console.log("currentTime");
+    lastRenderTime = currentTime;
+    update();
+    draw();
+}
+    
+    
+window.requestAnimationFrame(main);
+
+function update(){
+    updateSnake()
+    updateFood()
+    checkDeath();
+};
+
+function draw(){
+    gameBoard.innerHTML ="";
+    drawSnake(gameBoard);
+    drawFood(gameBoard);
+
+};
+
+function checkDeath(){
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersect()
+}
---
+++ food.js
@@ -0,0 +1,33 @@
+
+import { onSnake, expandSnake } from "./snake.js";
+import { randomGridPosition } from "./grid.js";
+
+let food = getRandomFooadPosition()
+const EXPANSION_RATE = 1;
+
+
+export function update(){
+    if(onSnake(food)){
+        expandSnake(EXPANSION_RATE);
+        food = getRandomFooadPosition()
+    }
+    
+}
+
+export function draw(gameBoard){
+  
+    const foodElement = document.createElement("div")
+    foodElement.style.gridRowStart = food.y
+    foodElement.style.gridColumnStart = food.x
+    foodElement.classList.add("food");
+    gameBoard.appendChild(foodElement)
+  
+}
+
+function getRandomFooadPosition(){
+    let newfoodPosition;
+    while(newfoodPosition==null || onSnake(newfoodPosition)){
+        newfoodPosition = randomGridPosition();
+    }
+    return newfoodPosition;
+}
